6c6b2c0a9a1441bc1f309260237151a56d6bdc12
MudOnTire <895157882@qq.com>
2022-07-26 12:24:48 +0800

    update

--- client/FileUploader.js
+++ client/FileUploader.js
@@ -1,7 +1,14 @@
+const TASK_STATUS = {
+  PROCESSING: 1,
+  SUCCESS: 2,
+  ERROR: 3,
+}
+
 class FileUploader {
   constructor({
     element,
-    uploadUrl
+    uploadUrl,
+    renderTask
   }) {
     if (element instanceof HTMLElement) {
       this.element = element;
@@ -9,6 +16,7 @@ class FileUploader {
       throw new Error('element should be an HTMLElement')
     }
     this.uploadUrl = uploadUrl;
+    this.renderTask = renderTask;
     this.#init();
   }
 
@@ -21,8 +29,9 @@ class FileUploader {
   }
 
   #listenToEvents = () => {
-    this.element.addEventListener('drop', this.#handleDrop);
-    this.element.addEventListener('dragover', this.#handleDragover);
+    const dropAreaDOM = this.element.querySelector('.drop-area');
+    dropAreaDOM.addEventListener('drop', this.#handleDrop);
+    dropAreaDOM.addEventListener('dragover', this.#handleDragover);
   }
 
   #handleDrop = (e) => {
@@ -55,24 +64,30 @@ class FileUploader {
   #upload = (file) => {
     const data = new FormData();
     data.append('file', file);
-    // fetch(this.uploadUrl, {
-    //   method: 'POST',
-    //   body: data
-    // })
-    //   .then(res => res.json())
-    //   .then((res) => {
-    //     console.log(`${file.name} upload succeeded`, res)
-    //   })
-    //   .catch(() => {
-    //     console.log(`${file.name} upload failed`)
-    //   });
-
+    const task = {
+      name: file.name,
+      status: TASK_STATUS.PROCESSING,
+      progress: 0
+    }
+    this.tasks.push(task);
     const xhr = new XMLHttpRequest();
     xhr.open('POST', this.uploadUrl);
     xhr.upload.addEventListener('progress', (e) => {
-      console.log('progress', e);
+      const { loaded, total } = e;
+      const progress = Math.round(loaded / total * 100);
+      task.progress = progress;
+      this.#renderTasks();
     });
     xhr.send(data);
   }
 
+  #renderTasks = () => {
+    const taskListDOM = this.element.querySelector('.task-list');
+    taskListDOM.innerHTML = '';
+    const sorted = this.tasks.sort((a, b) => a.status - b.status);
+    for (const task of sorted) {
+      console.log('render task', task);
+      taskListDOM.append(this.renderTask(task));
+    }
+  }
 }
--- client/script.js
+++ client/script.js
@@ -1,4 +1,24 @@
+const processingTemplate = document.querySelector('#template-task-processing');
+const successTemplate = document.querySelector('#template-task-success');
+const errorTemplate = document.querySelector('#template-task-error');
+
 new FileUploader({
   element: document.querySelector('.dnd-file-uploader'),
-  uploadUrl: 'http://localhost:3000/upload'
+  uploadUrl: 'http://localhost:3000/upload',
+  renderTask: function (task) {
+    switch (task.status) {
+      case TASK_STATUS.PROCESSING: {
+        const taskDOM = processingTemplate.content.firstElementChild.cloneNode(true);
+        const nameDOM = taskDOM.querySelector('.task-name');
+        const progressDOM = taskDOM.querySelector('.task-progress');
+        nameDOM.textContent = task.name;
+        const progress = `${task.progress}%`
+        progressDOM.textContent = progress;
+        taskDOM.style.background = `linear-gradient(to right, #bae7ff ${progress}, #fafafa ${progress}, #fafafa 100%)`
+        return taskDOM;
+      }
+      default:
+        break;
+    }
+  }
 });
